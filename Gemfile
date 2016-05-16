a5affcd27a4f858a1815e678fb9b27029b22c50a
Ohad Perry <ohadpartuck@gmail.com>
2016-05-16 23:28:28 +0300

    fix

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 
-ruby '2.1.5'
+ruby '2.1.2'
 
 # lightweight framework
 gem 'sinatra', '~> 1.4', :require => 'sinatra/base'
--- main.rb
+++ main.rb
@@ -1,7 +1,7 @@
 require 'bundler'
 Bundler.require
 
-
+@validation_token = 'kkkooo'
 class PerfectGift < Sinatra::Base
   get '/' do
     'Hello world!'
@@ -13,7 +13,25 @@ class PerfectGift < Sinatra::Base
     "Hello #{params['name']}!"
   end
 
+  get '/webhook/' do
+    if @request.params['hub.verify_token'] == @validation_token
+      @request.params['hub.challenge']
+    else
+      'Error, wrong validation token'
+    end
+
+
+  end
+  # app.get('/webhook/', function (req, res) {
+  #    if (req.query['hub.verify_token'] === '<validation_token>') {
+  #        res.send(req.query['hub.challenge']);
+  #    }
+  #    res.send('Error, wrong validation token');
+  #    })
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
+
+
 
