9c80ac9faf150d729fd6f6f041ac30bb1399e9fa
Ohad Perry <ohadpartuck@gmail.com>
2016-05-19 22:56:08 +0300

    auto_commit

--- main.rb
+++ main.rb
@@ -1,7 +1,28 @@
 require 'bundler'
 Bundler.require
+require 'net/http'
+
 
 $validation_token = 'kkkooo'
+
+
+def send_text_message(sender, text)
+  message_data = {
+      text:text
+  }
+
+  post_data = Net::HTTP.post_form(
+      URI.parse('https://graph.facebook.com/v2.6/me/messages'),
+      {
+          recipient: {id:sender},
+          message: message_data,
+      }
+  )
+
+  puts post_data.body
+
+
+end
 
 class PerfectGift < Sinatra::Base
   get '/' do
@@ -23,7 +44,17 @@ class PerfectGift < Sinatra::Base
   end
 
   post '/webhook-perfect-gift/' do
-    @request.params
+    messaging_events = @request.params.entry[0].messaging
+
+    for i in 0..(messaging_events.length -1)
+        event = messaging_events[i]
+        sender = event.sender.id
+        if event.message && event.message.text
+          text = event.message.text
+          send_text_message(sender, "Text received, echo: "+ text)
+          # // Handle a text message from this sender
+        end
+    end
   end
 
 
