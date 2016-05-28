7a4153c1ff1451dc1091ecfcd591f072e69ac083
Ohad Perry <ohadpartuck@gmail.com>
2016-05-28 12:29:09 +0300

    adding

--- config.ru
+++ config.ru
@@ -2,6 +2,8 @@ require 'bundler'
 Bundler.require
 
 require 'facebook/messenger'
+require_relative 'main'
 require_relative 'bot'
 
 map('/bot') { run Facebook::Messenger::Server }
+map('/') { run PerfectGift }
--- main.rb
+++ main.rb
@@ -15,28 +15,6 @@ Facebook::Messenger.configure do |config|
   config.verify_token = $validation_token
 end
 
-def send_text_message(sender, text)
-  message_data = {
-      text:text
-  }
-  post_data = HTTParty.post("https://graph.facebook.com/v2.6/me/messages?access_token=#{$page_access_token}",
-                body: {
-                    recipient: {id:sender},
-                    message: message_data,
-                }).body
-  # post_data = Net::HTTP.post_form(
-  #     URI.parse('https://graph.facebook.com/v2.6/me/messages?access_token='+$page_access_token),
-  #     {
-  #         recipient: {id:sender},
-  #         message: message_data,
-  #     }
-  # )
-
-  puts post_data
-
-
-end
-
 class PerfectGift < Sinatra::Base
 
   before do
@@ -45,41 +23,8 @@ class PerfectGift < Sinatra::Base
 
 
   get '/' do
-    'Hello world!'
-  end
-
-  get '/hello/:name' do
-    # matches "GET /hello/foo" and "GET /hello/bar"
-    # params['name'] is 'foo' or 'bar'
-    "Hello #{params['name']}!"
-  end
-
-  get '/webhook-perfect-gift/' do
-    puts "get webhook-perfect-gift!"
-    puts @request.params.inspect
-    puts "get webhook-perfect-gift! end"
-    if @request.params['hub.verify_token'] == $validation_token
-      @request.params['hub.challenge']
-    else
-      'Error, wrong validation token'
-    end
-  end
-
-  post '/webhook-perfect-gift/' do
-    content_type :json
-    params = JSON.parse(request.body.read)
-
-    messaging_events = params['entry'][0]['messaging']
-
-    for i in 0..(messaging_events.length - 1)
-        event = messaging_events[i]
-        sender = event['sender']['id']
-        if event['message'] && event['message']['text']
-          text = event['message']['text']
-          send_text_message(sender, "Text received, echo: "+ text)
-          # // Handle a text message from this sender
-        end
-    end
+    # File.read(File.join('website', 'index.html'))
+    erb 'website/index.html'
   end
 
 
